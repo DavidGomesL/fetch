cb97adbada8528575a6dd0b90616f9e28fa2afc9
DavidGomesL <david0596@digitalcollegesul.local>
2024-04-25 17:02:36 -0300

    adicionado comentarios no arquivo card.js

--- card.js
+++ card.js
@@ -1,16 +1,19 @@
-fetch("https://rickandmortyapi.com/api/character")
-.then(Response => Response.json())
-.then(data => {
-    let rowCard = document.querySelector(".rowCard")
+//primeira forma de trabalhar com fetch
+fetch("https://rickandmortyapi.com/api/character") //Pega a API
+.then(Response => Response.json())//Converte em JSON
+.then(data => {//Cria os dados para pecorrer os dados da API
+    let rowCard = document.querySelector(".rowCard") //Seleciona a div containerCard, fixa no HTML
+    //For: pecorre os dados dos arrays em loop
     for (let index = 0; index < data.results.length; index++) {
-        if(index % 4 === 0) {
-            let newRow = document.createElement("div");
-                        newRow.classList.add("row", "row-cols-1", "row-cols-md-4", "g-4"); // Bootstrap grid classes
-                        rowCard.appendChild(newRow);
+        if(index % 4 === 0) { //Condiciona 4 cards por linha
+            let newRow = document.createElement("div"); //Cria nova div dentro da div containerCard, fixa do html
+                        newRow.classList.add("row", "row-cols-1", "row-cols-md-4", "g-4"); //Cria classes que pertencem ao bootstrap grid
+                        rowCard.appendChild(newRow); //Aplica a estrutura no containerCard
         }
 
-        let cardCol = document.createElement("div")
-        cardCol.classList.add("col"); // Bootstrap column class
+        let cardCol = document.createElement("div") //Cria uma nova div
+        cardCol.classList.add("col"); // Cria classe do bootstrap column
+        //cardCol.innerHTML: Vai ser criado o card na div
         cardCol.innerHTML = `
         <div class="card" style="width: 18vw;">
                             <img id="cardImg" class="card-img-top" src="${data.results[index].image}" alt="Card image cap">
@@ -21,11 +24,11 @@ fetch("https://rickandmortyapi.com/api/character")
         </div>                    
         `
         
-        rowCard.lastChild.appendChild(cardCol)
+        rowCard.lastChild.appendChild(cardCol) //adiciona um novo card como um filho na ultima div de coluna na linha cards, para que os cards sejam adicionados sequencialmente a linha conforme eles são gerados.
         
     }
 })
-.catch(error => console.log(error))
+.catch(error => console.log(error)) //Interrompe o codigo caso o mesmo apresente erro e mostra a mensagem presente no console.
 
 /* <tr>
                                 <th scope="row">${data.results[index].id}</th>
